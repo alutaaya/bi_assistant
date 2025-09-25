f18448fbbba81df0736d59e87810b0ded735aceb
alutaaya <108323515+alutaaya@users.noreply.github.com>
2025-09-25 13:18:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -33,8 +33,9 @@ from langgraph.graph import StateGraph, END
 from typing import TypedDict, List, Any, Dict, Optional
 
 # PandaAI
-from pandasai import PandasAI
+###from pandasai import PandasAI
 from pandasai.llm.openai import OpenAI
+from pandasai_litellm.litellm import LiteLLM
 ##from pandasai import SmartDataframe
 import pandasai as pai
 ###from pandasai.smart_data_frame import SmartDataframe
