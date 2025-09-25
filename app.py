8b04b2c22da074c22c5267df0c6c48265da47b65
alutaaya <108323515+alutaaya@users.noreply.github.com>
2025-09-25 15:55:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ from typing import TypedDict, List, Any, Dict, Optional
 from pandasai import PandasAI
 from pandasai.llm.openai import OpenAI
 ###from pandasai_litellm.litellm import LiteLLM
-##from pandasai import SmartDataframe
+from pandasai import SmartDataframe
 import pandasai as pai
 ###from pandasai.smart_data_frame import SmartDataframe
 
